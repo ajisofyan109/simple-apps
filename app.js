23bd1df122a7a53c0c8fb062fcc3074451bac44f
ajisofyan109 <ajisofyanm3@gmail.com>
2024-03-03 13:32:27 +0700

    update

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get('/app1', (req, res) => {
 });
 
 app.get('/about', (req, res) => {
-  res.send('Wellcome, This is new Project..')
+  res.send('Wellcome, This is new Project')
 });
 
 app.get('/users', (req, res, next) => {
