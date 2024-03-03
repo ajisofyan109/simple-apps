62eb36fb3eade9a6ae7779c3014330e8186535b4
ajisofyan109 <ajisofyanm3@gmail.com>
2024-03-03 11:53:09 +0700

    update

--- app.js
+++ app.js
@@ -20,18 +20,14 @@ app.get('/app1', (req, res) => {
   res.send('Hello this Apps 1!')
 });
 
-app.get('/home', (req, res) => {
+app.get('/about', (req, res) => {
   res.send('Wellcome, This is new Project..')
 });
 
 app.get('/users', (req, res, next) => {
   const sql = "SELECT * FROM tb_data ORDER BY id desc"
   connection.query(sql,(error, fields) => {
-    if (error) {
-      console.log('error', error)
-    } else {
       res.send(fields)
-    }
   })
 });
 
